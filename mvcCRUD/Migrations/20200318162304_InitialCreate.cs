b25c85d7f7d80093295f9b55505757d73f76ff66
Neftali Oviedo <benjaminoviedo52@gmail.com>
2020-03-19 16:50:08 -0400

    version 1

---
+++ mvcCRUD/Migrations/20200318162304_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mvcCRUD.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FullName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    EmpCode = table.Column<string>(type: "varchar(10)", nullable: true),
+                    Position = table.Column<string>(type: "varchar(100)", nullable: true),
+                    OfficeLocation = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.EmployeeID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+        }
+    }
+}
